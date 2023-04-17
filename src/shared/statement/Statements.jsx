2a78a8d304fba567fd632749d8e581b15468392e
GigaPok <gigapokera@gmail.com>
2023-04-18 02:16:21 +0400

    update

--- src/shared/statement/Statements.jsx
+++ src/shared/statement/Statements.jsx
@@ -1,35 +1,190 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Statements.scss";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
 import StatementCard from "./statementcard/StatementCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 export function Statements() {
   const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+  const [show, setShow] = useState(false);
+
+  const [stepTwo, setStepTwo] = useState(false);
+  const [stepThree, setStepThree] = useState(false);
+
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
+  const twoStep = () => {
+    setShow(false);
+    setStepTwo(true);
+  };
+
+  const threStep = () => {
+    setStepTwo(false);
+    setStepThree(true);
+  };
+
+  const addKeyword = () => {};
+
   return (
-    <div className="statements">
-      <div className="leftbar">
-        <div className="top-card">
-          <div className="tittle-addpost">
-            <h3>განცხადებები</h3>
-            <button>
-              <FontAwesomeIcon icon={faPlus} />
-              დამატება
-            </button>
+    <>
+      <div className="statements">
+        <div className="leftbar">
+          <div className="top-card">
+            <div className="tittle-addpost">
+              <h3>განცხადებები</h3>
+              <button onClick={() => setShow(true)}>
+                <FontAwesomeIcon icon={faPlus} />
+                სიახლის განთავსება
+              </button>
+            </div>
+            <div className="fast-search">
+              <NavLink to="/">აქტუალური</NavLink>
+              <NavLink to="/s">ახალი გამოქყვეყნებული</NavLink>
+              <NavLink to="/ss">დამახსოვრებული</NavLink>
+            </div>
           </div>
-          <div className="fast-search">
-            <NavLink to="/">აქტუალური</NavLink>
-            <NavLink to="/s">ახალი გამოქყვეყნებული</NavLink>
-            <NavLink to="/ss">დამახსოვრებული</NavLink>
-          </div>
+          {array.map((el) => (
+            <StatementCard />
+          ))}
         </div>
-        {array.map((el) => (
-          <StatementCard />
-        ))}
+        <div className="sidebar"></div>
+
+        {show && (
+          <div className="modal-add-post" data-aos="flip-left">
+            <form action="/action_page.php">
+              <div className="wrapper">
+                <h2 className="border-bottom">სიახლის სწრაფი დამატება</h2>
+                <div class="wrapper-inputs">
+                  <input
+                    type="text"
+                    placeholder="სათაური"
+                    name="title"
+                    id="title"
+                    required
+                  />
+                  <textarea
+                    type="text"
+                    placeholder="ინფორმაცია"
+                    name="information"
+                    id="information"
+                    required
+                    rows={5}
+                  />
+                </div>
+                <div className="button-wrapper border-top">
+                  <button onClick={() => setShow(false)}>გათიშვა</button>
+                  <button onClick={() => twoStep()} class="registerbtn">
+                    დამასხოვრება
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        )}
+        {stepTwo && (
+          <div className="modal-add-post" data-aos="flip-left">
+            <form action="/action_page.php">
+              <div className="wrapper">
+                <h2 className="border-bottom">მეტი ინფორმაციის დამატება</h2>
+                <div class="wrapper-inputs">
+                  <input
+                    type="text"
+                    placeholder="სახელი, გვარი"
+                    name="title"
+                    id="title"
+                  />
+
+                  <input
+                    type="number"
+                    placeholder="მობილურის ნომერი"
+                    name="phone"
+                    id="phone"
+                    rows={5}
+                  />
+                </div>
+                <label className="border-top ptop-25">
+                  დაუმატე TAG - ები, რომ უფრო მეტმა ნახოს
+                </label>
+                <div className="keyword-wrapper">
+                  <span className="keyword">
+                    <FontAwesomeIcon icon={faPlus} />
+                    ხელოსანი
+                  </span>
+                  <span className="keyword">
+                    <FontAwesomeIcon icon={faPlus} />
+                    ონკანის ხელოსანი
+                  </span>
+                  <span className="keyword">
+                    <FontAwesomeIcon icon={faPlus} />
+                    პროფესიონალი
+                  </span>
+                </div>
+                <input
+                  type="hidden"
+                  placeholder="tags"
+                  name="keywords"
+                  id="keywords"
+                />
+                <div className="button-wrapper border-top">
+                  <button
+                    onClick={() => alert("სიახლე წარმატებით გამოქვეყნდა")}
+                  >
+                    გამოქვეყნება
+                  </button>
+                  <button onClick={() => threStep()} class="registerbtn">
+                    ინფორმაციის დამატება
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        )}
+
+        {stepThree && (
+          <div className="modal-add-post" data-aos="flip-left">
+            <form>
+              <div className="wrapper">
+                <h2 className="border-bottom">ბოლო ეტაპი</h2>
+                <div class="wrapper-inputs">
+                  <input
+                    type="text"
+                    placeholder="eMail სურვილისამებრ"
+                    name="title"
+                    id="title"
+                  />
+                  <input
+                    type="text"
+                    placeholder="პაროლი"
+                    name="password"
+                    id="password"
+                  />
+                  <input
+                    type="text"
+                    placeholder="გაიმეორე პაროლი"
+                    name="passwordrepeat"
+                    id="passwordrepeat"
+                    rows={5}
+                  />
+                </div>
+                <div className="button-wrapper border-top">
+                  <button onClick={() => twoStep()}>შევსებულის ნახვა</button>
+                  <button
+                    onClick={() => alert("სიახლე წარმატებით გამოქვეყნდა")}
+                  >
+                    გამოქვეყნება
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        )}
       </div>
-      <div className="sidebar"></div>
-    </div>
+    </>
   );
 }
--- src/shared/statement/statementcard/StatementCard.jsx
+++ src/shared/statement/statementcard/StatementCard.jsx
@@ -1,46 +1,192 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faLocationDot,
   faHeart,
   faThumbsDown,
+  faArrowLeft,
+  faUpRightFromSquare,
+  faTag,
+  faUserNurse,
+  faUserTie,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import "./StatementCard.scss";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 export default function StatementCard() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
   return (
-    <div className="statement-card">
-      <div className="wrapper-top">
-        <h2>ვეძებ ხელოსანს, რომელიც დამიმონტაჟებს ონკანს</h2>
-        <div>
-          <span>
-            <FontAwesomeIcon icon={faThumbsDown} />
-          </span>
-          <span>
-            <FontAwesomeIcon icon={faHeart} />
-          </span>
+    <>
+      <div className="statement-card" onClick={() => setShow(true)}>
+        <div className="wrapper-top">
+          <h2>ვეძებ ხელოსანს, რომელიც დამიმონტაჟებს ონკანს</h2>
+          <div>
+            <span>
+              <FontAwesomeIcon icon={faThumbsDown} />
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faHeart} />
+            </span>
+          </div>
+        </div>
+        <div className="info-on-statemet">
+          <span>ფასი შეთანხმებით</span>
+          <span>ნინი გოგიძე</span>
+          <span>სავარაუდო ბიუჯეტი: 50 GEL</span>
+          <span>გამოქვეყნებულია 5 საათის წინ</span>
+        </div>
+        <p>
+          გამარჯობა! ვეძებ ხელოსანს, რომელიც დამიმონტაჟებს ონკანს. მჭირდება
+          დამონტაჟება სასწრაფოდ, ვისაც გცალიათ მხოლოდ ისენი გამომეხმაურეთ.
+          მადლობა წინასწარ
+        </p>
+        <div className="tags">
+          <Link>ონკანის ხელოსანი</Link>
+          <Link>ონკანის მონტაჟი</Link>
+          <Link>თბილისი</Link>
+        </div>
+        <div className="location">
+          <FontAwesomeIcon icon={faLocationDot} /> თბილისი, ვაკე-საბურთალო
         </div>
       </div>
-      <div className="info-on-statemet">
-        <span>ფასი შეთანხმებით</span>
-        <span>ნინი გოგიძე</span>
-        <span>სავარაუდო ბიუჯეტი: 50 GEL</span>
-        <span>გამოქვეყნებულია 5 საათის წინ</span>
-      </div>
-      <p>
-        გამარჯობა! ვეძებ ხელოსანს, რომელიც დამიმონტაჟებს ონკანს. მჭირდება
-        დამონტაჟება სასწრაფოდ, ვისაც გცალიათ მხოლოდ ისენი გამომეხმაურეთ. მადლობა
-        წინასწარ
-      </p>
-      <div className="tags">
-        <Link>ონკანის ხელოსანი</Link>
-        <Link>ონკანის მონტაჟი</Link>
-        <Link>თბილისი</Link>
-      </div>
-      <div className="location">
-        <FontAwesomeIcon icon={faLocationDot} /> თბილისი, ვაკე-საბურთალო
-      </div>
-    </div>
+      {show && (
+        <div className="modal">
+          <div className="modal-wrapper">
+            <div className="opacity" onClick={() => setShow(false)}></div>
+            <div className="content" data-aos="fade-left">
+              <div className="back-newtab">
+                <FontAwesomeIcon
+                  onClick={() => setShow(false)}
+                  icon={faArrowLeft}
+                />
+                <a href="/" target="_blank">
+                  <FontAwesomeIcon icon={faUpRightFromSquare} />
+                  ახალ თაბში გახსნა
+                </a>
+              </div>
+              <div className="work-about">
+                <div>
+                  <div className="hed-title pad-20 border-bottom border-right">
+                    <h3>გვჭირდება სასწრაფოდ ონკანის ხელოსანი</h3>
+                    <span className="different">კომპანია ვანი</span>
+                    <span className="date">გამოქვეყნებულია 45 წუთის წინ</span>
+                    <span>
+                      <FontAwesomeIcon icon={faLocationDot} />
+                      თბილისი
+                    </span>
+                  </div>
+                  <div className="about pad-20 border-bottom border-right">
+                    <h2>გვჭირდება 5 ხელოსანი</h2>
+                    <p>
+                      გამარჯობა, კომპანია ვანს ესაჭიროვება 5 ხელოსანი. გვჭირდება
+                      მხოლოდ გამოცდილები. დასამონტაჟებელი არის 1000 ონკანი.
+                      <br />
+                      <br />
+                      გვესაჭიროვება სასწრაფოდ, შეგვეხმიანეთ მხოლოდ ის
+                      ადამიანები, რომლებიც მალევე შეძლებენ დაწყებას.
+                      <br />
+                      <br />
+                      გადახდა შესძლებელია 2 ნაწილადად.
+                      <br />
+                      <br />
+                      დაგვიკავშირდით მაილზე ან ნომერზე
+                    </p>
+                  </div>
+                  <div className="price pad-20 border-bottom border-right">
+                    <div className="box">
+                      <span className="hed-title">
+                        <FontAwesomeIcon icon={faTag} />
+                        საფასური
+                      </span>
+                      <span>ფასი შეთანხმებით</span>
+                    </div>
+                    <div className="box">
+                      <span className="hed-title">
+                        <FontAwesomeIcon icon={faUserNurse} />
+                        გამოცდილება
+                      </span>
+                      <span>პროფესიონალი</span>
+                    </div>
+                    <div className="box">
+                      <span className="hed-title">
+                        <FontAwesomeIcon icon={faUserTie} />
+                        დამკვეთი
+                      </span>
+                      <span>კომპანია</span>
+                    </div>
+                  </div>
+                  <div className="pad-20 border-bottom border-right statement-status">
+                    <span>
+                      განცხადება:
+                      <span>აქტიურია</span>
+                    </span>
+                  </div>
+                  <div className="pad-20 border-right theme">
+                    <h2>თემატიკა</h2>
+                    <div className="tags">
+                      <a href="/" target="_blank">
+                        ხელოსანი
+                      </a>
+                      <a href="/" target="_blank">
+                        თბილისი
+                      </a>
+                      <a href="/" target="_blank">
+                        ონკანის შეკეთება
+                      </a>
+                      <a href="/" target="_blank">
+                        ონკანის ხელოსანი
+                      </a>
+                      <a href="/" target="_blank">
+                        გამოცდილი ხელოსანი
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div className="aside">
+                  <div className="client-info-top pad-20 border-bottom">
+                    <button>დარეკვა</button>
+                    <button className="save-button">
+                      <span>
+                        <FontAwesomeIcon icon={faHeart} />
+                        შენახვა
+                      </span>
+                    </button>
+                    <span>შპს ვანი</span>
+                    <span>კომპანია</span>
+                  </div>
+                  <div className="pad-20 user-info border-bottom">
+                    <h2>მომხმარებლის შესახებ</h2>
+                    <span>
+                      მარკეტების ქსელი „გვირილა“ ინოვაციური ბრენდია საქართველოს
+                      ბაზარზე, რომლის მიზანია შესთავაზოს სხვადასხვა გემოვნების
+                      მომხმარებელს მაღალი ხარისხის, ხელმისაწვდომი ფასისა და
+                      ფართო ასორტიმენტის პროდუქცია მთელი ქვეყნის მასშტაბით.
+                      ამჟამად კომპანია არის სწრაფად მზარდი, რომლისთვისაც
+                      მნიშვნელოვანია თანამშრომლის კარიერულ და პროფესიულ
+                      განვითარებაზე ზრუნვა.
+                    </span>
+                    <span className="hed-title">მდებარეობა</span>
+                    <span>თბილისი</span>
+                    <span className="hed-title">გრაფიკი</span>
+                    <span>ორშ-პარ</span>
+                    <span className="hed-title">წევრი</span>
+                    <span> 2022 წლის 19 ოქტომბრიდან</span>
+                  </div>
+                </div>
+              </div>
+              <div></div>
+            </div>
+          </div>
+          {(document.body.style.height = "100vh")}
+        </div>
+      )}
+    </>
   );
 }
